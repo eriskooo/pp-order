5664b2ac252859b28d38e90bf1bd18aa5d634136
eriskooo <lorman@pobox.sk>
2019-08-22 16:02:35 +0200

    ukazka testu convert

--- customer-service/src/test/java/sk/pazurik/customerservice/domain/product/ProductDTOTest.java
+++ customer-service/src/test/java/sk/pazurik/customerservice/domain/product/ProductDTOTest.java
@@ -1,6 +1,35 @@
 package sk.pazurik.customerservice.domain.product;
 
-// otestovat prevod entity -> dto
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ProductDTOTest {
 
+    @Test
+    public void convertDTOtoEntity() {
+        ProductDTO dto = TestProduct.PRODUCT_1_DTO();
+        ProductEntity entity = new ProductEntity(dto);
+
+        assertThat(dto.getDescription()).isEqualTo(entity.getDescription());
+        assertThat(dto.getEan()).isEqualTo(entity.getEan());
+        assertThat(dto.getId()).isEqualTo(entity.getId());
+        assertThat(dto.getName()).isEqualTo(entity.getName());
+        assertThat(dto.getPrice_w_VAT()).isEqualTo(entity.getPrice_w_VAT());
+        assertThat(dto.getPrice_wo_VAT()).isEqualTo(entity.getPrice_wo_VAT());
+    }
+
+    @Test
+    public void convertEntityToDTO() {
+        ProductEntity entity = TestProduct.PRODUCT_1_ENTITY();
+        ProductDTO dto = new ProductDTO(entity);
+
+        assertThat(dto.getDescription()).isEqualTo(entity.getDescription());
+        assertThat(dto.getEan()).isEqualTo(entity.getEan());
+        assertThat(dto.getId()).isEqualTo(entity.getId());
+        assertThat(dto.getName()).isEqualTo(entity.getName());
+        assertThat(dto.getPrice_w_VAT()).isEqualTo(entity.getPrice_w_VAT());
+        assertThat(dto.getPrice_wo_VAT()).isEqualTo(entity.getPrice_wo_VAT());
+    }
 }
