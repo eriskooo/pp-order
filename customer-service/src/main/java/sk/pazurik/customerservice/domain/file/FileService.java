09ac943017048b6cbb9d32865a1633738947e2d1
eriskooo <lorman@pobox.sk>
2019-08-24 12:38:21 +0200

    javadoc

--- customer-service/src/main/java/sk/pazurik/customerservice/domain/file/FileService.java
+++ customer-service/src/main/java/sk/pazurik/customerservice/domain/file/FileService.java
@@ -1,11 +1,34 @@
 package sk.pazurik.customerservice.domain.file;
 
 public interface FileService {
+    /**
+     * nahra chujovinu.
+     *
+     * @param file
+     * @return
+     */
     FileDTO saveFile(byte[] file);
-    
+
+    /**
+     * updatene cosi
+     *
+     * @param id
+     * @param file
+     */
     void updateFile(Long id, byte[] file);
-    
+
+    /**
+     * zmaze cosi
+     *
+     * @param id
+     */
     void deleteFile(Long id);
 
+    /**
+     * cosi nacita
+     *
+     * @param id
+     * @return
+     */
     byte[] getFileById(final Long id);
 }
